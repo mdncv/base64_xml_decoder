c762085e631a4fa24541a5bf93ae4d09ba9d7d2b
mdncv <m.v.medencev@gmail.com>
2023-08-28 20:11:12 +0300

    feat: added encoding detection

--- main.py
+++ main.py
@@ -5,8 +5,28 @@ import xml.dom.minidom
 import base64
 
 
-def decode_base64_to_windows1251(base64_string):
-    return base64.b64decode(base64_string).decode('windows-1251')
+def detect_encoding(base64_string):
+    buffer = base64.b64decode(base64_string)
+
+    if b'<?xml' not in buffer:
+        return 'utf-8'
+
+    encoding_start = buffer.find(b'encoding=')
+
+    if encoding_start == -1:
+        return 'utf-8'
+
+    encoding_start += 10
+    encoding_end = buffer.find(b'"', encoding_start)
+
+    if encoding_end == -1:
+        return 'utf-8'
+
+    return buffer[encoding_start:encoding_end].decode("utf-8")
+
+
+def decode_base64(base64_string, encoding):
+    return base64.b64decode(base64_string).decode(encoding)
 
 
 def make_xml_pretty(xml_string):
@@ -25,17 +45,19 @@ def choose_filename():
     return input('Choose filename: ')
 
 
-def save_xml_to_file(xml_text):
-    with open(f'{choose_directory()}/{choose_filename()}.xml', 'w', encoding='windows-1251') as file:
+def save_xml_to_file(xml_text, encoding):
+    with open(f'{choose_directory()}/{choose_filename()}.xml', 'w', encoding=encoding) as file:
         file.write(xml_text)
     print('XML saved')
 
 
 def main():
-    decoded_string = decode_base64_to_windows1251(input('Enter the Base64-encoded string: '))
+    encoded_string = input('Enter the Base64-encoded XML document: ')
+    encoding = detect_encoding(encoded_string)
+    decoded_string = decode_base64(encoded_string, encoding)
     print(decoded_string)
     print(make_xml_pretty(decoded_string)) if input('Make pretty? y/N: ').lower() == 'y' else None
-    save_xml_to_file(decoded_string) if input('Save file? y/N: ').lower() == 'y' else None
+    save_xml_to_file(decoded_string, encoding) if input('Save file? y/N: ').lower() == 'y' else None
 
 
 if __name__ == '__main__':
